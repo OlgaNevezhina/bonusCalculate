f1bf268c0901a8c3c55b3ed847383bb7ec084adb
Olga Nevezina <nevezhinao@yandex.ru>
2020-11-29 22:06:59 +0300

    Update tests

--- src/test/java/BonusServiceTest.java
+++ src/test/java/BonusServiceTest.java
@@ -5,7 +5,7 @@ import static org.junit.jupiter.api.Assertions.*;
 class BonusServiceTest {
 
     @Test
-    void calculate() {
+    void shouldCalculateRegisteredUnderLimit() {
         BonusService service = new BonusService();
 
         // подготавливаем данные:
@@ -17,5 +17,47 @@ class BonusServiceTest {
         long actual = service.calculate(amount, registered);
 
       assertEquals(expected, actual);
+    }
+    @Test
+    void shouldCalculateRegisteredOverLimit() {
+        BonusService service = new BonusService();
+
+        // подготавливаем данные:
+        long amount = 1_000_000_60;
+        boolean registered = true;
+        long expected = 500;
+
+        // вызываем целевой метод:
+        long actual = service.calculate(amount, registered);
+
+        assertEquals(expected, actual);
+    }
+    @Test
+    void shouldCalculateNotRegisteredUnderLimit() {
+        BonusService service = new BonusService();
+
+        // подготавливаем данные:
+        long amount = 1000_60;
+        boolean registered = false;
+        long expected = 10;
+
+        // вызываем целевой метод:
+        long actual = service.calculate(amount, registered);
+
+        assertEquals(expected, actual);
+    }
+    @Test
+    void shouldCalculateNotRegisteredOverLimit() {
+        BonusService service = new BonusService();
+
+        // подготавливаем данные:
+        long amount = 1_000_000_60;
+        boolean registered = false;
+        long expected = 500;
+
+        // вызываем целевой метод:
+        long actual = service.calculate(amount, registered);
+
+        assertEquals(expected, actual);
     }
 }
